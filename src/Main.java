a245cd569cbc3fe7654acf38f3d88a0106e326db
TheJim03 <jimmy.karrie@alumni.fh-aachen.de>
2023-10-30 10:41:06 +0100

    lol

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,35 @@ import Bank.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Erstellung von Payment-Objekten unter Verwendung verschiedener Konstruktoren
+        Payment payment1 = new Payment("2023-10-29", 100.0, "Eingehende Zahlung", 0.02, 0.01);
+        Payment payment2 = new Payment(payment1); // Kopierkonstruktor
+        Payment payment3 = new Payment("2023-10-29", -50.0, "Ausgehende Zahlung", 0.02, 0.01);
 
-        Payment payment1 = new Payment("01.01.2021", 100, "Gehalt");
-        Payment payment2 = new Payment("02.01.2021", -50, "Miete");
-        Payment payment3 = new Payment(payment1);
-        Payment payment4 = new Payment("03.01.2021", 100, "Gehalt", 0.1, 0.2);
-        Payment payment5 = new Payment("04.01.2021", -50, "Miete", 0.1, 0.2);
-        Payment payment6 = new Payment(payment4);
-        Transfer transfer1 = new Transfer("05.01.2021", 100, "Gehalt", "Max", "Moritz");
-        Transfer transfer2 = new Transfer("06.01.2021", -50, "Miete", "Moritz", "Max");
-        Transfer transfer3 = new Transfer(transfer1);
-        Transfer transfer4 = new Transfer("07.01.2021", 100, "Gehalt", "Max", "Moritz");
-        Transfer transfer5 = new Transfer("08.01.2021", -50, "Miete", "Moritz", "Max");
-        Transfer transfer6 = new Transfer(transfer4);
+        // Erstellung von Transfer-Objekten unter Verwendung verschiedener Konstruktoren
+        Transfer transfer1 = new Transfer("2023-10-29", 500.0, "Geldüberweisung", "Hase", "Erklärbär");
+        Transfer transfer2 = new Transfer(transfer1); // Kopierkonstruktor
+        Transfer transfer3 = new Transfer("2023-10-29", -200.0, "Negative Überweisung", "Hase2", "Erklärbär2");
 
+        // Testen der calculate() Methode
+        System.out.println("Berechnungen:");
+        System.out.println("Berechneter Betrag für Payment 1: " + payment1.calculate());
+        System.out.println("Berechneter Betrag für Payment 2: " + payment2.calculate());
+        System.out.println("Berechneter Betrag für Payment 3: " + payment3.calculate());
+        System.out.println("Berechneter Betrag für Transfer 1: " + transfer1.calculate());
+        System.out.println("Berechneter Betrag für Transfer 2: " + transfer2.calculate());
+        System.out.println("Berechneter Betrag für Transfer 3: " + transfer3.calculate());
 
-        System.out.println(payment1.calculate());
-        System.out.println(payment2.calculate());
-        System.out.println(payment3.calculate());
-        System.out.println(payment4.calculate());
-        System.out.println(payment5.calculate());
-        System.out.println(payment6.calculate());
-        System.out.println(transfer1.calculate());
-        System.out.println(transfer2.calculate());
-        System.out.println(transfer3.calculate());
-        System.out.println(transfer4.calculate());
-        System.out.println(transfer5.calculate());
-        System.out.println(transfer6.calculate());
+        // Testen der equals() Methode
+        System.out.println("\nGleichheitsprüfungen:");
+        System.out.println("Payment 1 ist gleich Payment 2: " + payment1.equals(payment2));
+        System.out.println("Payment 1 ist gleich Payment 3: " + payment1.equals(payment3));
+        System.out.println("Transfer 1 ist gleich Transfer 2: " + transfer1.equals(transfer2));
+        System.out.println("Transfer 1 ist gleich Transfer 3: " + transfer1.equals(transfer3));
 
+        // Testen der toString() Methode
+        System.out.println("\nZeichenkettenrepräsentationen:");
+        System.out.println("Test Payment 1:\n" + payment1.toString() + "\n");
+        System.out.println("Test Transfer 1:\n" + transfer1.toString());
     }
 }
